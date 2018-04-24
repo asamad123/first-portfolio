e28a01d7938d7382095a95a46dd0d9d9d9439f33
Abdus samad <abdussamad@Abduss-MBP.home>
2018-04-24 12:23:37 +0100

    Added navbar and made it responsive.

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-// Adjust the "querySelector" value to target your image
-var header = document.querySelector("#showcase");
-document.body.classList.add('js-loading');
-header.addEventListener("load", removeLoadingClass);
-function removeLoadingClass() {
-  document.body.classList.remove('js-loading');
-}
+// if the window is over 680 pixels in height add class and remove if the the height is less than 640px
+$(document).ready(function () {
+    var nav = $('#nav');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 680) {
+            nav.addClass("navbar", "animateJS");
+        } else {
+            nav.removeClass("navbar");
+        }
+    });
+});
+
+
+// hamburger animation
+$(document).ready(function(){
+    $(".hamburger").click(function(){
+      $(this).toggleClass("is-active");
+    });
+  });
+  
+// add and remove class for navbar on mobile sizes
+  $(document).ready(function(){
+    $("#hamburger").click(function(){
+        $("ul").toggleClass("addNav");
+    });
+});
